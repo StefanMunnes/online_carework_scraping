1a2f109bef764eef9c80295370231234c7ae04fa
Stefan Munnes <munnes@wzb.eu>
2025-07-19 01:07:41 +0200

    working scraper: add all plz; create big loop; add messages; close new popups; use profile cards and unique ids

--- scripts/scraping_helpling.R
+++ scripts/scraping_helpling.R
@@ -9,13 +9,39 @@ source("scripts/scraping_fcts.R")
 
 # set variables to change for the search query
 query_values <- list(
-  plz = c("Leipzig" = "04315", "Bremen" = "28211"),
+  plz = c(
+    "Hamburg - Mitte" = "20095",
+    "Hamburg - Nord" = "22415",
+    "Hamburg - Ost" = "22149",
+    "Hamburg - Süd" = "21073",
+    "Hamburg - West" = "22765",
+    "Berlin - Mitte" = "10117",
+    "Berlin - Nord" = "13156",
+    "Berlin - Ost" = "12683",
+    "Berlin - Süd" = "12107",
+    "Berlin - West" = "14052",
+    "München - Mitte" = "80331",
+    "München - Nord" = "80937",
+    "München - Ost" = "81825",
+    "München - Süd" = "81547",
+    "München - West" = "81245",
+    "Stuttgart" = "70174",
+    "Leipzig" = "04315",
+    "Bremen" = "28211"
+  ),
   frequency = "Einmalig",
   duration = "2 Stunden",
   date = "22"
 )
 
-query_date <- format(Sys.time(), "%Y%m%d_%H%M")
+plz_done <- c(
+  "München - Mitte" = "80331",
+  "Hamburg - Mitte" = "20095",
+  "Hamburg - Nord" = "22415",
+  "Leipzig" = "04315",
+  "Bremen" = "28211",
+  "Stuttgart" = "70174"
+)
 
 # define constant objects for scraping
 target_url <- "https://www.helpling.de/"
@@ -78,36 +104,22 @@ rD <- rsDriver(
 remDr <- rD$client
 
 # set timeout
-remDr$setTimeout(type = "implicit", milliseconds = 3000)
+remDr$setTimeout(type = "implicit", milliseconds = 5000)
 remDr$setTimeout(type = "page load", milliseconds = 5000)
 
 # set window size for mobile (otherwise page is not loading correct)
 remDr$setWindowSize(width = 700, height = 900)
 
-
 # navigate to target page
 remDr$navigate(target_url)
 
-# enter PLZ and click enter
-remDr$findElement('css', '#bid_postcode')$sendKeysToElement(list(
-  query_values[["plz"]][["Bremen"]],
-  key = "enter"
-))
+Sys.sleep(3)
 
 # accept cookie banner
-try(remDr$findElement("css", elements["cookie_accept"])$clickElement(), TRUE)
-
-
-# check for wrong redirect if no results for PLZ
-# if (!grepl("step-zero", remDr$getCurrentUrl())) {
-#   message("Redirect to wrong page. Move to previous page.")
-#   remDr$goBack()
-# } else {
-#   message("Redirect to correct page.")
-# }
-
-# Close popup explanation
-remDr$findElement("xpath", "//div[text() = 'Überspringen']")$clickElement()
+try(
+  remDr$findElement("css", elements["cookie_accept"])$clickElement(),
+  silent = TRUE
+)
 
 # Wait and close Rabatt popup
 Sys.sleep(10)
@@ -116,216 +128,309 @@ try(
     "xpath",
     "//button[contains(@class, 'CloseButton')]"
   )$clickElement(),
-  TRUE
+  silent = TRUE
 )
 
-# Choos eone of three frequency Button
-remDr$findElement(value = elements["frequency_choice"])$clickElement()
+# for (plz in query_values[["plz"]][!(query_values[["plz"]] %in% plz_done)]) {
+for (plz in c("22415")) {
+  query_date <- format(Sys.time(), "%Y%m%d_%H%M")
 
-# Close popup: "regelmäßige Buchungen"
-remDr$findElement("xpath", "//div[text() = 'Schließen']")$clickElement()
+  # enter PLZ and click enter
+  message("Enter PLZ: ", plz, " and click enter.")
+  remDr$findElement('css', '#bid_postcode')$sendKeysToElement(list(
+    plz,
+    key = "enter"
+  ))
 
-# Move to next page
-remDr$findElement("xpath", "//div[text() = 'Fortfahren']")$clickElement()
+  # check for wrong redirect if no results for PLZ
+  # if (!grepl("step-zero", remDr$getCurrentUrl())) {
+  #   message("Redirect to wrong page. Move to previous page.")
+  #   remDr$goBack()
+  # } else {
+  #   message("Redirect to correct page.")
+  # }
 
-# Move to next Month
-# remDr$findElement("xpath", "//*[@id='arrow-left']")$clickElement()
+  message("Wait 7 seconds for page to load.")
+  Sys.sleep(7)
 
-# Pick date
-remDr$findElement("xpath", elements["date_input"])$clickElement()
+  # Close popup explanation
+  message("Close popup explanation.")
+  remDr$findElement("xpath", "//div[text() = 'Überspringen']")$clickElement()
 
-# Move to next page (keep time (12:00) constant)
-remDr$findElement("xpath", "//div[text() = 'Fortfahren']")$clickElement()
+  Sys.sleep(2)
 
-# Pick duration
-remDr$findElement(value = elements["duration_input"])$clickElement()
+  # Choos eone of three frequency Button
+  message("Choose frequency.")
+  remDr$findElement(value = elements["frequency_choice"])$clickElement()
 
+  # Close popup: "regelmäßige Buchungen"
+  message("Close popup: 'regelmässige Buchungen'")
+  remDr$findElement("xpath", "//div[text() = 'Schließen']")$clickElement()
 
-Sys.sleep(2)
+  # Move to next page
+  remDr$findElement("xpath", "//div[text() = 'Fortfahren']")$clickElement()
 
-# Extract number of results from html page
-number_cleaners <- remDr$findElement(
-  "xpath",
-  elements["cleaners"] # "//div[contains(text(), 'Reinigungskräfte verfügbar')]"
-)$getElementText() |>
-  str_extract("\\d+") |>
-  as.integer()
+  # Move to next Month
+  # remDr$findElement("xpath", "//*[@id='arrow-left']")$clickElement()
 
+  # Pick date
+  remDr$findElement("xpath", elements["date_input"])$clickElement()
 
-# ---- Scrape profiles ----
+  # Move to next page (keep time (12:00) constant)
+  remDr$findElement("xpath", "//div[text() = 'Fortfahren']")$clickElement()
 
-# create empty data frames for profiles and reivews (append in loop)
-df_profiles_all <- data.frame()
-df_reviews_all <- data.frame()
+  # Pick duration
+  remDr$findElement(value = elements["duration_input"])$clickElement()
 
-index <- 1 # # index of the next profile to click
-seen_ids <- character(0) # store all scraped profile ids
+  Sys.sleep(2)
 
+  # If asked for, click to see all profiles (instead of ask the top rated)
+  message("Click to see all profiles.")
+  suppressMessages(try(
+    remDr$findElement(
+      "xpath",
+      "//div[text() = 'Jetzt Profile vergleichen']"
+    )$clickElement(),
+    silent = TRUE
+  ))
 
-while (length(seen_ids) < number_cleaners) {
-  # 1.1 load most recent list of profile cards and create IDs
-  profile_cards <- remDr$findElements("xpath", elements["profile_cards"])
-  profile_ids <- sapply(profile_cards, function(card) card$getElementText()) |>
-    str_remove_all("[^A-Za-z0-9]")
+  Sys.sleep(3)
+  suppressMessages(try(
+    remDr$findElement(
+      "xpath",
+      "//div[text() = 'Los geht’s!']"
+    )$clickElement(),
+    silent = TRUE
+  ))
 
-  # 1.2 load most recent list of profiles and keep if not seen
-  profiles_elements <- remDr$findElements("xpath", elements["profile_details"])
-  profiles_not_seen <- profiles_elements[!(profile_ids %in% seen_ids)]
+  # Extract number of results from html page
+  number_cleaners <- remDr$findElement(
+    "xpath",
+    elements["cleaners"] # "//div[contains(text(), 'Reinigungskräfte verfügbar')]"
+  )$getElementText() |>
+    str_extract("\\d+") |>
+    as.integer()
 
-  # 2. scrape new profiles: loop over profile elements and extract data
-  message(length(profiles_not_seen), " new Profiles to scrape ...")
-  for (profile in profiles_not_seen) {
-    # 2.1 open profile page
-    profile$clickElement()
-    Sys.sleep(sample(c(0.5, 1, 1.5), 1))
+  # ---- Scrape profiles ----
 
-    # 2.2 ---- Extract profile data ----
-    profile_html <- remDr$getPageSource()[[1]] |>
-      read_html() |>
-      html_element("div[data-testid='CandidateModal']")
+  # create empty data frames for profiles and reivews (append in loop)
+  df_profiles_all <- data.frame()
+  df_reviews_all <- data.frame()
 
-    df_profile <- data.frame(
-      name = profile_html |>
-        html_node(xpath = "div/div/div[2]") |>
-        html_text(),
-      place = profile_html |>
-        html_node(xpath = "div/div[3]/div/div/div[1]/div[1]") |>
-        html_text(),
-      price = profile_html |>
-        html_node(xpath = "div/div[3]/div/div/div[1]/div[2]/div") |>
-        html_text() |>
-        str_extract("[0-9.]+") |>
-        as.numeric(),
-      rating = profile_html |>
-        html_node(xpath = "div/div[3]/div/div/div[2]/div[1]/div") |>
-        html_text() |>
-        as.numeric(),
-      ratings = profile_html |>
-        html_node(xpath = "div/div[3]/div/div/div[2]/div[3]/div/div[1]/div") |>
-        html_text() |>
-        str_extract("\\d+") |>
-        as.integer(),
-      hires = profile_html |>
-        html_node(xpath = "div/div[3]/div/div/div[2]/div[2]/div") |>
-        html_text() |>
-        str_extract("\\d+") |>
-        as.integer(),
-      checklist = profile_html |>
-        html_element(
-          xpath = "//div[contains(text(), 'Checkliste:')]/following-sibling::div[2]"
-        ) |>
-        html_nodes("div") |>
-        html_text() |>
-        unique() |>
-        paste(collapse = ":|;"),
-      about_text = html_node(
-        profile_html,
-        xpath = "//div[contains(text(), 'Über mich:')]/following-sibling::div[2]"
-      ) |>
-        html_text(),
-      img_src = profile_html |>
-        html_node("img") |>
-        html_attr("src"),
-      query_date = query_date,
-      query_plz = query_values[["plz"]][["Bremen"]]
-    ) |>
-      mutate(
-        id = paste(
-          str_replace_all(name, " ", "_"),
-          as.character(price),
-          as.character(rating),
-          as.character(hires),
-          sep = "_"
+  index <- 1 # # index of the next profile to click
+  seen_ids <- character(0) # store all scraped profile ids
+
+  repeat {
+    # 1.1 load most recent list of profile cards and create IDs
+    profile_cards <- remDr$findElements("xpath", elements["profile_cards"])
+    profile_ids <- sapply(profile_cards, function(card) {
+      card$getElementText()
+    }) |>
+      str_remove_all("[^A-Za-z0-9]")
+
+    # 1.2 load most recent list of profiles and keep if not seen
+    profiles_not_seen <- profile_cards[!(profile_ids %in% seen_ids)]
+
+    # End scraping if there are not new profiles to scrape
+    if (length(profiles_not_seen) == 0) {
+      message("No new profiles to scrape.")
+      break
+    }
+
+    # 2. scrape new profiles: loop over profile elements and extract data
+    message(length(profiles_not_seen), " new Profiles to scrape ...")
+
+    for (profile_card in profiles_not_seen) {
+      profile_id <- profile_card$getElementText() |>
+        str_remove_all("[^A-Za-z0-9]")
+
+      profile_detail_button <- profile_card$findElement(
+        "xpath",
+        elements["profile_details"]
+      )
+
+      # 2.1 open profile page (try twice, otherwhise skip)
+      open_success <- FALSE
+      for (try_i in seq_len(2)) {
+        # 1) attempt to click
+        try(profile_detail_button$clickElement(), silent = TRUE)
+        Sys.sleep(sample(c(1, 1.5, 2), 1))
+
+        # 2) test whether the “close” button is there
+        open_success <- !inherits(
+          suppressMessages(try(
+            remDr$findElement("xpath", elements["profile_close"]),
+            silent = TRUE
+          )),
+          "try-error"
         )
+
+        if (open_success) {
+          # we broke through!
+          break
+        } else if (try_i < 2) {
+          message(sprintf("⚠️ Open attempt %d failed, retrying...", try_i))
+        }
+      }
+
+      if (!open_success) {
+        message("❌ Failed to open profile after 2 attempts; skipping.")
+        seen_ids <- c(seen_ids, profile_id)
+        index = index + 1
+        next
+      }
+
+      # 2.2 ---- Extract profile data ----
+      profile_html <- remDr$getPageSource()[[1]] |>
+        read_html() |>
+        html_element("div[data-testid='CandidateModal']")
+
+      df_profile <- data.frame(
+        name = profile_html |>
+          html_node(xpath = "div/div/div[2]") |>
+          html_text(),
+        place = profile_html |>
+          html_node(xpath = "div/div[3]/div/div/div[1]/div[1]") |>
+          html_text(),
+        price = profile_html |>
+          html_node(xpath = "div/div[3]/div/div/div[1]/div[2]/div") |>
+          html_text() |>
+          str_extract("[0-9.]+") |>
+          as.numeric(),
+        rating = profile_html |>
+          html_node(xpath = "div/div[3]/div/div/div[2]/div[1]/div") |>
+          html_text() |>
+          as.numeric(),
+        ratings = profile_html |>
+          html_node(
+            xpath = "div/div[3]/div/div/div[2]/div[3]/div/div[1]/div"
+          ) |>
+          html_text() |>
+          str_extract("\\d+") |>
+          as.integer(),
+        hires = profile_html |>
+          html_node(xpath = "div/div[3]/div/div/div[2]/div[2]/div") |>
+          html_text() |>
+          str_extract("\\d+") |>
+          as.integer(),
+        checklist = profile_html |>
+          html_element(
+            xpath = "//div[contains(text(), 'Checkliste:')]/following-sibling::div[2]"
+          ) |>
+          html_nodes("div") |>
+          html_text() |>
+          unique() |>
+          paste(collapse = ":|;"),
+        about_text = html_node(
+          profile_html,
+          xpath = "//div[contains(text(), 'Über mich:')]/following-sibling::div[2]"
+        ) |>
+          html_text(),
+        img_src = profile_html |>
+          html_node("img") |>
+          html_attr("src"),
+        query_date = query_date,
+        query_plz = plz
+      ) |>
+        mutate(
+          id = paste(
+            str_replace_all(name, " ", "_"),
+            as.character(price),
+            as.character(rating),
+            as.character(hires),
+            sep = "_"
+          )
+        )
+
+      # 2.1 add profile data to df
+      df_profiles_all <- rbind(df_profiles_all, df_profile)
+
+      # 2.3 ---- Extract reviews ----
+      # 2.3.1 Get elements from review list
+      rev_elements <- profile_html |>
+        html_elements(
+          xpath = "//div[contains(text(), 'Bewertungen:')]/following-sibling::div"
+        )
+
+      # 2.3.2 Extract review data
+      df_reviews <- tibble(
+        id = df_profile$id,
+        rev_name = rev_elements |>
+          html_nodes(xpath = "div[1]/div[1]") |>
+          html_text(),
+        rev_rating = rev_elements |>
+          html_nodes(xpath = "div[1]/div[2]/div[6]") |>
+          html_text(),
+        revtext = rev_elements |>
+          html_nodes(xpath = "div[2]") |>
+          html_text(),
+        query_date = query_date,
+        query_plz = plz
       )
 
-    # 2.1 add profile data to df
-    df_profiles_all <- rbind(df_profiles_all, df_profile)
+      # 2.3.3 add review data to df
+      df_reviews_all <- bind_rows(df_reviews_all, df_reviews)
 
-    # 2.3 ---- Extract reviews ----
-    # 2.3.1 Get elements from review list
-    rev_elements <- profile_html |>
-      html_elements(
-        xpath = "//div[contains(text(), 'Bewertungen:')]/following-sibling::div"
+      message(
+        "Profile ",
+        index,
+        ": ",
+        df_profile$name,
+        " with ",
+        nrow(df_reviews),
+        " reviews"
       )
 
-    # 2.3.2 Extract review data
-    df_reviews <- tibble(
-      id = df_profile$id,
-      rev_name = rev_elements |>
-        html_nodes(xpath = "div[1]/div[1]") |>
-        html_text(),
-      rev_rating = rev_elements |>
-        html_nodes(xpath = "div[1]/div[2]/div[6]") |>
-        html_text(),
-      revtext = rev_elements |>
-        html_nodes(xpath = "div[2]") |>
-        html_text(),
-      query_date = query_date,
-      query_plz = query_values[["plz"]][["Bremen"]]
-    )
+      # 2.4 add id of new profile to seen ids and increment index
+      seen_ids <- c(seen_ids, profile_id)
+      index = index + 1
 
-    # 2.3.3 add review data to df
-    df_reviews_all <- bind_rows(df_reviews_all, df_reviews)
+      # 2.5 close/leave profile page
+      remDr$findElement("xpath", elements["profile_close"])$clickElement()
+      Sys.sleep(sample(c(1, 1.25, 1.5), 1))
+    }
 
+    if (length(seen_ids) != nrow(df_profiles_all)) {
+      message("Seen ids and profiles do not match")
+    }
+
+    # 4. scroll down to load new profiles
     message(
-      "Profile ",
-      index,
-      ": ",
-      df_profile$name,
-      " with ",
-      nrow(df_reviews),
-      " reviews"
+      length(seen_ids),
+      " Profiles of ",
+      number_cleaners,
+      " scraped, scroll down for more ..."
     )
-
-    index = index + 1
-
-    # 2.4close/leave profile page
-    remDr$findElement("xpath", elements["profile_close"])$clickElement()
-    Sys.sleep(sample(c(0.5, 1, 1.5), 1))
+    element_body <- remDr$findElement("css", "body")
+    element_body$sendKeysToElement(list(key = "end"))
+    Sys.sleep(3)
   }
 
-  # 3. add ids of new profiles to seen ids
-  seen_ids <- c(seen_ids, profile_ids) |> unique()
-
-  # 4. scroll down to load new profiles
-  message(
-    length(seen_ids),
-    " Profiles of ",
-    number_cleaners,
-    " scraped, scroll down for more ..."
+  # 5. save data
+  dataframes <- list(
+    "profiles" = df_profiles_all,
+    "reviews" = df_reviews_all
   )
-  element_body <- remDr$findElement("css", "body")
-  element_body$sendKeysToElement(list(key = "end"))
-  Sys.sleep(1)
-}
 
+  for (data in names(dataframes)) {
+    message("Saving ", data)
+    dataframes[[data]] |>
+      write.csv(
+        file = sprintf(
+          "data/scraping/helpling/%s_%s_%s.csv",
+          data,
+          plz,
+          query_date
+        ),
+        row.names = FALSE
+      )
+  }
 
-# 5. save data
-dataframes <- list(
-  "profiles" = df_profiles_all,
-  "reviews" = df_reviews_all
-)
-
-for (data in names(dataframes)) {
-  message("Saving ", data)
-  dataframes[[data]] |>
-    write.csv(
-      file = sprintf(
-        "data/scraping/helpling/%s_%s_%s.csv",
-        data,
-        query_values[["plz"]][["Bremen"]],
-        query_date
-      ),
-      row.names = FALSE
-    )
-}
-
-
-# 6. go back
-for (i in 1:5) {
-  remDr$goBack()
-  Sys.sleep(sample(c(0.5, 1, 1.5), 1))
+  # 6. go back
+  for (i in 1:5) {
+    remDr$goBack()
+    Sys.sleep(sample(c(0.5, 1, 1.5), 1))
+  }
 }
 
 # close the browser
