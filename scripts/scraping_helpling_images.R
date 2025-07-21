4dfaab794a2a2dd180feaaa7ffef52de2cf99fd2
Stefan Munnes <munnes@wzb.eu>
2025-07-21 12:42:51 +0200

    scrape avatar images for helpling

---
+++ scripts/scraping_helpling_images.R
@@ -0,0 +1,43 @@
+profiles_all <- read.csv2(
+  "data/helpling/scraping/profiles_all.csv",
+  stringsAsFactors = FALSE
+)
+
+
+# get image urls (without default avatars)
+file_names <- unique(profiles_all$img_file[profiles_all$img_picture])
+
+for (file in file_names) {
+  file_path <- paste0("data/scraping/helpling/images/", file)
+
+  message("Process image: ", file, appendLF = FALSE)
+
+  if (file.exists(file_path)) {
+    message(" (File already exists)")
+    next
+  }
+
+  tryCatch(
+    {
+      download.file(
+        url = url,
+        destfile = paste0("data/scraping/helpling/images/", file),
+        mode = "wb"
+      )
+
+      message(" (Downloaded)")
+    },
+    error = function(e) {
+      message(" (Download failed)")
+    }
+  )
+
+  Sys.sleep(sample(c(0.5, 1, 1.5), 1))
+}
+
+
+download.file(
+  url = "https://h2-production-de.s3.eu-west-1.amazonaws.com/uploads/profile/picture/251832/avatar-1734971643620.png",
+  destfile = "tmp/avatar.png",
+  mode = "wb"
+)
